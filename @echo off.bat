eb6059db311a91df6755a8cb9be38dba9d816703
Frotrue <105928069+frotrue@users.noreply.github.com>
2025-09-24 12:16:37 +0900

    Update @echo off.bat

--- @echo off.bat
+++ @echo off.bat
@@ -1,11 +1,12 @@
 @echo off
 powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+set PATH=%USERPROFILE%\.cargo\bin;%PATH%
 cd C:\
 mkdir tetris_uv
 cd C:\tetris_uv
-uv init
 git clone https://github.com/frotrue/2345767iuyjgfhds.git
 cd 2345767iuyjgfhds
+uv init
 uv add tensorflow
 uv add tensorboard
 uv add keras
