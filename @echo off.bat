7bfaa26b8b06eb4c7f740740c7d9bb9e6860800c
Frotrue <105928069+frotrue@users.noreply.github.com>
2025-09-24 10:43:08 +0900

    Add files via upload

---
+++ @echo off.bat
@@ -0,0 +1,20 @@
+@echo off
+powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+cd C:\
+mkdir tetris_uv
+cd C:\tetris_uv
+uv init
+git clone https://github.com/frotrue/2345767iuyjgfhds.git
+cd 2345767iuyjgfhds
+uv add tensorflow
+uv add tensorboard
+uv add keras
+uv add opencv-python
+uv add numpy
+uv add Pillow
+uv add tqdm
+uv add onnxruntime
+uv add tf2onnx
+uv run run_model.py dqn_model.onnx
+
+pause
